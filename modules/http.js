4d00cb08b06a19a5f05470cab4f7a5d9bdb43535
mathznxs <math945374081@gmail.com>
2026-08-03 16:46:36 -0300

    first commit

---
+++ modules/http.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/home") {
+        res.writeHead(200, {"Content-type": "text-html"})
+        res.end("<h1>Hello World</h1>");
+    }
+    if (req.url === "/users") {
+        const users = [
+            {
+                name: "John Doe",
+                email: "john@doe.com"
+            },
+            {
+                name: "Jane Doe",
+                email: "jane@doe.com"
+            }
+        ]
+        res.writeHead(200, {"content-type": "application/json"})
+        res.end(JSON.stringify(users));
+    }
+})
+
+server.listen(port, () => console.log(`Rodando na porta ${port}`))
