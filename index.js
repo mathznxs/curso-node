ae4c34e735c11f5f252f537ff9399fbb0dd68d49
mathznxs <math945374081@gmail.com>
2026-08-03 22:16:04 -0300

    Acrescentando funções de criação de usuários, exibindo e atualizando dados do usuário exibidos direto do banco de dados e processados pelo servidor

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// const path = require ("./modules/path")
-// const fs = require("./modules/fs")
-
-// const http = require("./modules/http")
-
-// require("./modules/express")
 const dotenv = require("dotenv");
 const connectToDatabase = require("./src/database/connect")
 
 dotenv.config();
 connectToDatabase();
+// const path = require ("./modules/path")
+// const fs = require("./modules/fs")
+
+// const http = require("./modules/http")
+ 
+require("./modules/express")
--- modules/express.js
+++ modules/express.js
@@ -1,22 +1,51 @@
 const express = require("express")
+const UserModel = require("../src/models/user.model")
 
 const app = express();
+app.use(express.json())
 
-app.get("/home", (req, res) => {
-    res.status(200).send("<h1>Hello World!</h1>")
+// Exibindo usuário
+app.get("/users", async (req, res) => {
+    try {
+        const users = await UserModel.find({})
+        res.status(200).json(users) 
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+// Exibindo usuário por ID
+app.get("/users/:id", async (req, res) => {
+    try {   
+        const id = req.params.id;
+        const user = await UserModel.findById(id)
+
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
 })
-app.get("/users", (req, res) => {
-    const users = [
-        {
-            name: "Math",
-            email: "math@gmail.com",
-        },
-        {
-            name: "Nath",
-            email: "nath@gmail.com"
-        }
-    ]
-    res.status(200).json(users)
+
+// Criando usuário 
+app.post("/users", async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
+// Atualizando dados do usuário
+app.patch("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const users = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
+
+        return res.status(200).json(users);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 })
 
 const port = 8080;
